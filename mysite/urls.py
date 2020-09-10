ff8e8226c4ac44a173802115d7ba8fe57f91e7f7
Abhishek Kumar <abhishek.kumar@vmock.com>
2020-09-10 23:07:51 +0530

    Django Models and Django Admin

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,10 +17,10 @@ from django.contrib import admin
 from django.urls import path
 
 from personal.views import (
-	home_screen_view,
+    home_screen_view,
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_screen_view),
+    path('', home_screen_view, name="home"),
 ]
--- personal/views.py
+++ personal/views.py
@@ -1,22 +1,12 @@
 from django.shortcuts import render
+from personal.models import Question
 
 # Create your views here.
 
 def home_screen_view(request):
 	context = {}
-	# context['some_string'] = "this is some string that I'm passing to the view"
 
-	# context = {
-	# 	'some_string': "this is some string that I'm passing to the view"
-	# }
+	questions = Question.objects.all()
+	context['questions'] = questions
 
-	# context['some_number'] = 3151351
-
-	list_of_values = []
-	list_of_values.append("first entry")
-	list_of_values.append("second entry")
-	list_of_values.append("third entry")
-	list_of_values.append("fourth entry")
-	list_of_values.append("fifth entry")
-	context['list_of_values'] = list_of_values
-	return render(request, "personal/home.html", {})
+	return render(request, "personal/home.html", context)
