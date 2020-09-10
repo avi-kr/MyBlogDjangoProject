6e050e8bb1a1f3d481c98c41dbb8e0df58282fbc
Abhishek Kumar <abhishek.kumar@vmock.com>
2020-09-11 00:16:02 +0530

    Logging out Users and Checking Authentication Status

--- account/views.py
+++ account/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from account.forms import RegistrationForm
 
 
@@ -16,7 +16,13 @@ def registration_view(request):
 			return redirect('home')
 		else:
 			context['registration_form'] = form
-	else: #GET request
+
+	else:
 		form = RegistrationForm()
 		context['registration_form'] = form
 	return render(request, 'account/register.html', context)
+
+
+def logout_view(request):
+	logout(request)
+	return redirect('/')
