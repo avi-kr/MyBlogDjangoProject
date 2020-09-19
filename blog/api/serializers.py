b1f5f6f2fad15ccab8fc92858ee62be548084adf
Abhishek Kumar <abhishek.kumar@vmock.com>
2020-09-19 19:10:18 +0530

    Blog serializers class renamed

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from blog.models import BlogPost
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class BlogPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogPost
         fields = ['title', 'body', 'image', 'date_updated']
